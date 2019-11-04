517e8090bc1116f0aef36fe2f0e7ab4ef2a729ad
Andreas Lind <32731570+MandBoy@users.noreply.github.com>
2019-11-04 13:25:46 +0100

    Added our intellij project

---
+++ CodeProject/src/test/java/assignement/codeproject/CodeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package assignement.codeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
