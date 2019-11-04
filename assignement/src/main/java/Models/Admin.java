03a9fbe0194dc88d60af2f487439fdf720f0b8aa
Andreas Lind <32731570+MandBoy@users.noreply.github.com>
2019-11-04 15:03:26 +0100

    Added values to some models.

--- assignement/src/main/java/Models/Admin.java
+++ assignement/src/main/java/Models/Admin.java
@@ -1,4 +1,38 @@
 package Models;
 
-public class Admin {
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Admin{
+    @Id
+    private int id;
+    private String name;
+    private String email;
+
+    public Admin(){}
+    public Admin(int id, String name, String email){
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
--- assignement/src/main/java/Models/Student.java
+++ assignement/src/main/java/Models/Student.java
@@ -1,4 +1,35 @@
 package Models;
 
-public class Student {
+import javax.persistence.Entity;
+
+@Entity
+public class Student{
+    private int id;
+    private String name;
+    private int semester;
+
+    public Student(){}
+    public Student(int id, String name, int semester){
+        this.id = id;
+        this.name = name;
+        this.semester = semester;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getSemester() {
+        return semester;
+    }
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
 }
