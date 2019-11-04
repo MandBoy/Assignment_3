7a3c4d3cda362621f82264f53ccabf3e29c364a3
Andreas Lind <32731570+MandBoy@users.noreply.github.com>
2019-11-04 14:04:49 +0100

    Idk

--- assignment_3/src/main/java/com/example/assignment_3/Assignment3Application.java
+++ assignment_3/src/main/java/com/example/assignment_3/Assignment3Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Assignment3Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Assignment3Application.class, args);
     }
