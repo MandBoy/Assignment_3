8a7ec104103b08c1a118faa94cc46049e0800062
Andreas Lind <32731570+MandBoy@users.noreply.github.com>
2019-11-04 14:12:05 +0100

    Rip

--- assignment_3/src/main/java/com/example/assignment_3/Assignment3Application.java
+++ assignment_3/src/main/java/com/example/assignment_3/Assignment3Application.java
@@ -1,4 +1,4 @@
-package com.example.assignment_3;
+package java.com.example.assignment_3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,5 +8,4 @@ public class Assignment3Application {
     public static void main(String[] args) {
         SpringApplication.run(Assignment3Application.class, args);
     }
-
 }
