4c5df1e9dd1e3f3fbac9163610ea6096e28fc9f2
Leopold Berggreen Nguessan <leopold1@live.dk>
2019-11-11 11:23:56 +0100

    PersonRepo

--- assignement/src/main/java/Services/CRUDServices.java
+++ assignement/src/main/java/Services/CRUDServices.java
@@ -1,6 +1,15 @@
 package Services;
 
+import Repositories.TableRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CRUDServices{
+
+    @Autowired
+    TableRepo tableRepo;
+
     public void create(){}
     public void read(){}
     public void update(){}
