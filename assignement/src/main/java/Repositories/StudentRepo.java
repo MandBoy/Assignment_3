e4b793f17c55be461043cd7306773234bcc93abe
Leopold Berggreen Nguessan <leopold1@live.dk>
2019-11-25 13:58:42 +0100

    StudentRepo 2.0

--- assignement/src/main/java/Repositories/StudentRepo.java
+++ assignement/src/main/java/Repositories/StudentRepo.java
@@ -1,5 +1,47 @@
 package Repositories;
 
-public class StudentRepo{
+import Models.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class StudentRepo {
+
+    @Autowired
+    JdbcTemplate template;
+    String sql;
+    RowMapper<Student> rowMapper = new BeanPropertyRowMapper<>(Student.class);
+
+
+    public List<Student> fetchAllStudents(){
+        sql = "SELECT * FROM  Student";
+        return template.query(sql, rowMapper);
+    }
+
+    public void readStudentById(int id){
+        sql = "SELECT * FROM Student WHERE id=?";
+        template.queryForObject(sql, rowMapper, id);
+    }
+
+    public void createStudent(Student Student){
+        sql = "INSERT INTO Student(name, email, semester, login) VALUES(?, ?, ?, ?)";
+        template.update(sql, rowMapper, Student.getId(), Student.getName(), Student.getSemester(), Student.getLogin());
+    }
+
+    public void updateStudent(Student Student){
+        sql = "UPDATE Student SET name=?, SET email=?, SET login=? ";
+        template.update(sql, Student.getId(), Student.getName(), Student.getSemester(), Student.getLogin());
+    }
+
+    public void deleteStudent(int id){
+        sql = "DELETE FROM Student WHERE id=?";
+        template.update(sql, id);
+    }
+
 
 }
