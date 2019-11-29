403f8534f4e08f60513f4c62a318a6d1fa5bbd9c
Leopold Berggreen Nguessan <leopold1@live.dk>
2019-11-29 14:09:57 +0100

    SecurityConfig + Javax

--- assignement/src/main/java/Models/Student.java
+++ assignement/src/main/java/Models/Student.java
@@ -1,10 +1,16 @@
 package Models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.LinkedList;
 
 @Entity
 public class Student{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
     private String name;
     private String email;
